1374864eea559faf283e4338e9640b919baf17b2
qiepei.qp <qiepei.qp@alibaba-inc.com>
2014-12-07 13:13:03 +0800

    first commit

---
+++ tasks/qiepei_htmlmodify.js
@@ -0,0 +1,30 @@
+/*
+ * grunt-qiepei-htmlmodify
+ * https://github.com/qiepei/qiepei-htmlmodify
+ *
+ * Copyright (c) 2014 qiepei.qp
+ * Licensed under the MIT license.
+ */
+'use strict';
+
+module.exports = function (grunt) {
+    grunt.registerMultiTask('qiepei_htmlmin', 'Modified HTML', function () {
+        this.files.forEach(function (file) {
+            var src = file.src[0];
+
+            if (!grunt.file.exists(src || ' ')) {
+                return grunt.log.warn('Source file "' + src + '" not found.');
+            }
+
+            var input = grunt.file.read(src);
+
+            var output = input.replace(/<script\s.*<\/script>/ig,'');
+            var insertScript = '<script type="text/javascript" src="index.js"></script>'
+            output = output.replace(/<\/body>/ig, insertScript+'</body>');
+
+            grunt.file.write(file.dest, output);
+            grunt.log.writeln('Modified ' + file.dest);
+
+        });
+    });
+};
